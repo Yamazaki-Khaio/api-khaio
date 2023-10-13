abdf202ba43abb4946a6b618d4a9e5ae192aabc3
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-10-13 17:47:14 -0300

    feature

--- api-cardeneta/backend/controllers/alunosController.ts
+++ api-cardeneta/backend/controllers/alunosController.ts
@@ -1,14 +1,17 @@
 import Aluno from '../models/alunos';
+import { sequelize } from '../server';
 
 
 export default async function saveAlunos(aluno: Aluno) {
     try {
+        
         
         await Aluno.create(aluno as any);
         console.log("Aluno criado com sucesso!");
     } catch (error) {
         throw error;
     }
+
     
 }
 
