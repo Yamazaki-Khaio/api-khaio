8b3b96ebc080d013fd7a1839c60029387a7caad7
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-10-30 13:46:49 -0300

    init

--- projeto-gaia/src/app/app.component.spec.ts
+++ projeto-gaia/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-api'`, () => {
+  it(`should have as title 'projeto-gaia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-api');
+    expect(app.title).toEqual('projeto-gaia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-api app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-gaia app is running!');
   });
 });
--- projeto-gaia/src/app/app.component.ts
+++ projeto-gaia/src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'project-api';
-  items = ['item1', 'item2', 'item3', 'item4'];
+  title = 'projeto-gaia';
 }
