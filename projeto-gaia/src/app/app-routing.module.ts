5cd893bdb4551acb4117fd7f5596dc99105f8e9e
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-10-31 15:24:56 -0300

    feat

--- projeto-gaia/src/app/app-routing.module.ts
+++ projeto-gaia/src/app/app-routing.module.ts
@@ -1,26 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
+// carregando os modulos de rotas de cada feature
 const routes: Routes = [
-  {
-    path: 'users',
-    loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule)
-  },
-  {
-    path: 'products',
-    loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule)
-  },
-  {
-    path: 'orders',
-    loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule)
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-
+{path: '' , loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)},
 ];
 
 @NgModule({
