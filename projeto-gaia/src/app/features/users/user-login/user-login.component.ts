e4e10e1190f6a2b5f000cd9053f30b69c22d667d
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-11-03 10:15:24 -0300

    featuring register

--- projeto-gaia/src/app/features/users/user-login/user-login.component.ts
+++ projeto-gaia/src/app/features/users/user-login/user-login.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class UserLoginComponent {
 
+    constructor() { }
+
+    ngOnInit(): void {
+      
+
+    }
+
+
 }
--- projeto-gaia/src/app/features/users/user-registration/user-registration.component.ts
+++ projeto-gaia/src/app/features/users/user-registration/user-registration.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.sass']
 })
-export class UserRegistrationComponent {
+export class UserRegistrationComponent implements OnInit {
+  registrationForm!: FormGroup;
+  constructor(private fb: FormBuilder, private dataService: DataService) {}
 
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  registerUser(): void {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+      this.dataService.create('users', this.registrationForm.value).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
