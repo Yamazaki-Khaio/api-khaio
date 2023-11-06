674593594048430a6f83e615ccaaed2e745e05fd
Yamazaki-Khaio <kfonseca@ecomp.uefs.br>
2023-11-06 17:47:29 -0300

    init: backend

--- projeto-gaia/src/app/shared/components/header/header.component.ts
+++ projeto-gaia/src/app/shared/components/header/header.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent {
-  constructor() { }
-    Logo = "https://user-images.githubusercontent.com/90158519/229323866-f793144c-c11d-4326-bd73-340707257335.png";
-    isLogged = true;
-
-
-
+  constructor() {}
+  Logo =
+    'https://user-images.githubusercontent.com/90158519/229323866-f793144c-c11d-4326-bd73-340707257335.png';
+  isLogged = false;
 }
