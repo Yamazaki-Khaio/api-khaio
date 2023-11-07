b3ccfcb71529e1bb3420f3a91db475a234220809
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-11-07 15:35:15 -0300

    Delete unused files and code.

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+
+import express from 'express';
+
+// Cria uma instância do aplicativo Express
+const app = express();
+
+// Define uma rota de exemplo
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Inicia o servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+});
